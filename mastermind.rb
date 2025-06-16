efebbe005191ed2b3b6cead531169156db13fb22
bhagyeshsp <bhagyeshspathak@gmail.com>
2025-06-16 10:10:47 +0530

    layout the pseudocode

---
+++ mastermind.rb
@@ -0,0 +1,13 @@
+# There are total of 6 color pegs: 1,2,3,4,5,6
+# There are two colored feedback pegs: B, P, F (Bagel, Pico, Fermi)
+#   Bagel = black/red peg indicating that a color and its position right
+#   Pico = white peg indicating a color is right
+#   Fermi = blank holes indicating lack of right color and position
+# The computer generates a random color code [1,1,2,3]
+# The player begins his guessing: [1,1,1,1]
+# The computer provides feedback: 2B0P
+# The player adjusts his guess: [1,1,2,2]
+# The computer provides feedback: 3B0P
+# The player adjusts his guess: [1,1,2,3]
+# The computer provides feedback: 4B
+# You've won in 3 guesses!
