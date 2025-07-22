aacbf4fa161196dc45aa0a5048012de00a85a8d0
bhagyeshsp <bhagyeshspathak@gmail.com>
2025-07-22 22:51:48 +0530

    Better LinkedList like implementation
    - I had created a solution to create LinkedList using Arrays and I realized that it wasn't the spirit of the assignment.
    - This implementation matches what was expected.

--- project_linkedlist/implementation_v2/main.rb
+++ project_linkedlist/implementation_v2/main.rb
@@ -1,2 +1,30 @@
 require_relative "linkedlist"
 require_relative "node"
+
+list = LinkedList.new
+list.append("dog")
+p list
+list.append("cat")
+p list
+list.append("elephant")
+p list
+list.prepend("cow")
+p list
+p list.at(4)
+list.pop
+p list
+p list.contains?("abc")
+p list.find("cat")
+p list.find("abc")
+puts list
+list.insert_at("elephant", 2)
+puts list
+# list.insert_at("tiger", 1)
+# puts list
+list.remove_at(3)
+puts list
+# p list
+list.remove_at(2)
+puts list
+list.remove_at(3)
+puts list
--- project_linkedlist/implementation_v2/node.rb
+++ project_linkedlist/implementation_v2/node.rb
@@ -1,5 +1,7 @@
 # Contains methods for all Node objects
 class Node
+  attr_accessor :value, :next_node
+
   def initialize
     @value = nil
     @next_node = nil
