1a7cfe33b84fcee33859f4c97c92983fce2f6249
bhagyeshsp <bhagyeshspathak@gmail.com>
2025-07-29 08:53:02 +0530

    complete build_tree method
    - The main part of building and printing the tree is complete and tested
    
    Next:
    - Other methods such as insert, delete, etc..

--- balanced_bst/main.rb
+++ balanced_bst/main.rb
@@ -3,5 +3,6 @@ require_relative "node"
 
 test_tree = Tree.new
 p test_tree
-test_tree.build_tree([1, 2, 3, 4, 5])
+test_tree.build_tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
 p test_tree
+test_tree.pretty_print
--- balanced_bst/tree.rb
+++ balanced_bst/tree.rb
@@ -5,38 +5,33 @@ class Tree
 
   def build_tree(arr)
     sorted_arr = arr.uniq.sort
-    recursive_method(sorted_arr, 0, sorted_arr.length - 1)
+    mid = find_middle_index(0, sorted_arr.length - 1)
+    # Create root node
+    root_node = Node.new
+    root_node.value = sorted_arr[mid]
+    @root = root_node
+    root_node.left = recursive_method(sorted_arr, 0, mid - 1)
+    root_node.right = recursive_method(sorted_arr, mid + 1, sorted_arr.length - 1)
   end
 
-  # call this function with (start = 0) and (last = arr.length - 1) arguments
+  def find_middle_index(start, last)
+    (start + last) / 2
+  end
+
   def recursive_method(sorted_arr, start, last)
     return nil if start > last
 
-    # Find the middle element
-    mid = start + ((last - start) / 2)
-
-    if @root.nil?
-      # Create root node
-      root_node = Node.new
-      root_node.value = sorted_arr[mid]
-      @root = root_node
-    end
-
-    # Create left subtree
-    @root.left = recursive_method(sorted_arr, start, mid - 1)
-
-    # Create right subtree
-    @root.right = recursive_method(sorted_arr, mid + 1, last)
-
-    @root
+    mid = (start + last) / 2
+    new_node = Node.new
+    new_node.value = sorted_arr[mid]
+    new_node.left = recursive_method(sorted_arr, start, mid - 1)
+    new_node.right = recursive_method(sorted_arr, mid + 1, last)
+    new_node
   end
 
   def pretty_print(node = @root, prefix = "", is_left = true)
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
-    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
     pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
-
-  # [1, 2, 3, 4, 5, 6, 7]
-  # <TreeObj @root=<NodeObj @data=4 @left=<NodeObj @data=2 @left=<NodeObj @data=1 @left=nil @right=nil> @right=<NodeObj @data=3 @left=nil @right=nil>> @right=<NodeObj @data=6 @left=<NodeObj @data=5 @left=nil @right=nil>  @right=<NodeObj @data=7 @left=nil @right=nil>>>
 end
