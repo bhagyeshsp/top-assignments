57ac87c0623504d09f7d919b6ca723ad9a4a4cad
bhagyeshsp <bhagyeshspathak@gmail.com>
2025-07-31 11:57:10 +0530

    basic next steps calculations tested

--- project_knight_travails/knight.rb
+++ project_knight_travails/knight.rb
@@ -1,3 +1,18 @@
 # This class contains all methods for Knight objects
 class Knight
+  def knight_moves(origin, destination)
+  end
+
+  # returns an array of maximum possible moves addatives to X and Y positions:
+  # [[-1, -2], [-1, 2], [-2, -1], [-2, 1], [1, -2], [1, 2], [2, -1], [2, 1]]
+  # Any knight can move 2 moves forward and 1 moves side ways
+  def find_addatives(input = [-1, -2, 1, 2])
+    input.permutation(2).to_a.reject { |combo| combo[0].abs == combo[1].abs }
+  end
+
+  def find_next_possible_moves(position)
+    addatives = find_addatives.sort
+    unfiltered_moves = addatives.map { |combo| [position[0] + combo[0], position[1] + combo[1]] }
+    unfiltered_moves.reject { |combo| (combo[0].abs > 7) || (combo[1].abs > 7) }
+  end
 end
--- project_knight_travails/main.rb
+++ project_knight_travails/main.rb
@@ -1,1 +1,4 @@
 require_relative "knight"
+
+new_knight = Knight.new
+p new_knight.find_next_possible_moves([3, 2])
