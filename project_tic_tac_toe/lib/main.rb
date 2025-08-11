6ed28a17cbc0916a581adc0768ecf2f73aba853d
Bhagyesh Pathak <bhagyeshspathak@gmail.com>
2025-08-11 22:31:59 +0530

    fix: comment out code execution during testing

--- project_tic_tac_toe/lib/main.rb
+++ project_tic_tac_toe/lib/main.rb
@@ -1,9 +1,9 @@
 require_relative "player"
 require_relative "tic_tac_toe"
 
-new_game = TicTacToe.new
-new_game.initialize_game # Initializes the board
-new_game.set_players # Sets player names
-new_game.alot_sign
-new_game.orient_board
-new_game.loop_game
+# new_game = TicTacToe.new
+# new_game.initialize_game # Initializes the board
+# new_game.set_players # Sets player names
+# new_game.alot_sign
+# new_game.orient_board
+# new_game.loop_game
