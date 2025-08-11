3f063b47ef37453ef1e0d9ebb8322b576b311ed4
Bhagyesh Pathak <bhagyeshspathak@gmail.com>
2025-08-11 14:08:34 +0530

    test: survey all methods and create a skeleton with commentary
    I prefer this way of writing tests as it allows me to understand what is important and what is not.

--- project_tic_tac_toe/spec/player_spec.rb
+++ project_tic_tac_toe/spec/player_spec.rb
@@ -1,1 +1,13 @@
 require_relative "../lib/player"
+
+describe Player do
+  describe "#initialize" do
+    # Initialize methods that only create instance variables, don't need to be tested
+    # No need to be tested
+  end
+
+  describe "#declare_victory" do
+    # This is a Protected method
+    # No need to test it
+  end
+end
--- project_tic_tac_toe/spec/tic_tac_toe_spec.rb
+++ project_tic_tac_toe/spec/tic_tac_toe_spec.rb
@@ -1,1 +1,77 @@
 require_relative "../lib/tic_tac_toe"
+
+describe TicTacToe do
+  # For the flow of the methods in this class, I'm referring to the main.rb file
+  # Because it is responsible for orchestrating the game
+  # I will be testing only the 1st and 2nd degree of methods invoked by it
+  describe "#initialize_game" do
+    # If #initialization method only creates instance variables, no need to test
+    # BUT this method additionally commands other method -> create_new_method
+    # So, needs to be tested
+  end
+
+  describe "#set_players" do
+    # This is a Public Comand method
+    # Needs to be tested
+  end
+
+  describe "#alot_sign" do
+    # This is a Public Command method
+    # Needs to be tested
+  end
+
+  describe "#orient_board" do
+    # This method only contains puts statements
+    # No need to test
+  end
+
+  describe "#loop_game" do
+    # This is a Public Loop method
+    # Need to be tested
+  end
+
+  describe "#play_round" do
+    # This is a Public Loop method
+    # Need to be tested
+  end
+
+  describe "#count_round" do
+    # This is a Public Query method
+    # Need to be tested
+  end
+
+  describe "#current_player" do
+    # This is a Public Query method
+    # Need to be tested
+  end
+
+  describe "#get_input" do
+    # This is a Public Command method
+    # Need to be tested
+  end
+
+  describe "#update_board" do
+    # This is a Public Command method
+    # Need to be tested
+  end
+
+  describe "#update_log" do
+    # This is a Public Command method
+    # Need to be tested
+  end
+
+  describe "#display_board" do
+    # This method cotnains puts statements
+    # No need to test
+  end
+
+  describe "#check_victory" do
+    # This method is a Public Query method
+    # Needs to be tested
+  end
+
+  describe "#declar_draw" do
+    # This method contains puts statements
+    # No need to test
+  end
+end
