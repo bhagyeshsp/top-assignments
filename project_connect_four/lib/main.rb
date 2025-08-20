df59370f1772690d32c9aa65dbd1510bf79225ce
Bhagyesh Pathak <bhagyeshspathak@gmail.com>
2025-08-20 09:27:09 +0530

    setup: add basic pseudo-code for workflow

--- project_connect_four/lib/main.rb
+++ project_connect_four/lib/main.rb
@@ -1,2 +1,28 @@
 require_relative "connect_four"
 require_relative "player"
+
+# PSEUDOCODE
+# The game begins
+# It asks player-1 to choose their sprite
+# Then asks player-2 to choose their sprite
+# The game displays a 6*7 empty grid
+# The player-1 takes the first turn, choosing a cell by its XY coordinates
+# The game records this move
+# The player-2 takes the next turn, choosing a cell by its XY coordinates
+# The game records this move
+# The loop continues until there's a winning combo match
+
+#   +---+---+---+---+---+---+---+
+# 6 | o | o | o | o | o | o | o |
+#   +---+---+---+---+---+---+---+
+# 5 | o | o | o | o | o | o | o |
+#   +---+---+---+---+---+---+---+
+# 4 | o | o | o | o | o | o | o |
+#   +---+---+---+---+---+---+---+
+# 3 | o | o | o | o | o | o | o |
+#   +---+---+---+---+---+---+---+
+# 2 | o | o | o | o | o | o | o |
+#   +---+---+---+---+---+---+---+
+# 1 | o | o | o | o | o | o | o |
+#   +---+---+---+---+---+---+---+
+#     1   2   3   4   5   6   7
