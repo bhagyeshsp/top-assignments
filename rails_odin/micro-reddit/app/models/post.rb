9829429595ecb76ff54d14a3d5a398e6049c401b
bhagyeshsp <bhagyeshspathak@gmail.com>
2025-09-19 11:59:22 +0530

    db: setup Post model, add validations, run migration

---
+++ rails_odin/micro-reddit/app/models/post.rb
@@ -0,0 +1,5 @@
+class Post < ApplicationRecord
+  validates :body, presence: true
+  validates :user_id, presence: true
+  belongs_to :user
+end
--- rails_odin/micro-reddit/app/models/user.rb
+++ rails_odin/micro-reddit/app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true, length: { in: 6..20 }
+  has_many :posts
 end
